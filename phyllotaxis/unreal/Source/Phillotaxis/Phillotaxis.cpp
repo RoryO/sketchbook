8785cce07383caecc24f4e2663499beffdf11765
Rory O'Connell <rory.ocon@gmail.com>
2017-06-04 11:25:04 -0700

    adding unreal implementation

---
+++ phyllotaxis/unreal/Source/Phillotaxis/Phillotaxis.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Phillotaxis.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Phillotaxis, "Phillotaxis" );
