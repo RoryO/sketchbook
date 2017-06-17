086b8aae1730ffcdc6a3047daf076d925e7c6d93
Rory O'Connell <rory.ocon@gmail.com>
2017-06-17 12:02:02 -0700

    Adding history state for cells

--- game_of_life/p5/sketch.js
+++ game_of_life/p5/sketch.js
@@ -1,23 +1,25 @@
 class Cell {
   constructor(alive=false) {
+    this.stateHistory = [alive];
     this.alive = alive;
+    this.fullIntensity = 255;
+    this.steadyIntensity = 128;
+    this.deadIntensity = 0;
+    this.recentSwitchFrames = 0;
   }
 
   update() {
-    if(typeof this.newState !== 'undefined') {
-      this.alive = this.newState;
-      this.newState = undefined;
-    }
+    this.alive = this.stateHistory[0];
     return this;
   }
 
   revive() {
-    this.newState = true;
+    this.liveState = true;
     return this;
   }
 
   die() {
-    this.newState = false;
+    this.liveState = false;
     return this;
   }
 
@@ -27,15 +29,30 @@ class Cell {
     } else {
       this.revive();
     }
+    this.update();
     return this;
   }
 
-  toNumber() {
+  get toNumber() {
     if(this.alive) {
       return 1;
     } else {
       return 0;
     }
+  }
+
+  get intensity() {
+    const switched = this.stateHistory[0] !== this.stateHistory[1];
+    if (this.alive) {
+      return this.fullIntensity;
+    } else {
+      return this.deadIntensity;
+    }
+  }
+
+  set liveState(state) {
+    this.stateHistory.unshift(state);
+    this.stateHistory = this.stateHistory.slice(0, 30);
   }
 }
 
@@ -44,7 +61,7 @@ class Board {
     this.cells = []
     this.paused = true;
     this.cellSize = cellSize;
-    this.delay = 10;
+    this.delay = 5;
     this.totalCells = 0;
     this.grid = false;
 
@@ -65,8 +82,6 @@ class Board {
   }
 
   draw() {
-    stroke(128);
-
     for (let y = 0; y < this.cells.length; y++) {
       for (let x = 0; x < this.cells[y].length; x++) {
         const cell = this.cells[y][x];
@@ -74,12 +89,11 @@ class Board {
         if (!cell.alive && !this.grid) { continue; }
         beginShape();
         stroke(128);
-        if (cell.alive) { fill(128); }
-        else  { noFill(); }
+        fill(cell.intensity);
         rect(x * this.cellSize,
-              y * this.cellSize,
-              this.cellSize,
-              this.cellSize);
+             y * this.cellSize,
+             this.cellSize,
+             this.cellSize);
         endShape();
       }
     }
@@ -114,11 +128,11 @@ class Board {
         if (xoff < 0) { xoff = this.cells[yoff].length - 1; }
         else if (xoff >= this.cells.length) { xoff = 0; }
 
-        n += this.cells[yoff][xoff].toNumber();
+        n += this.cells[yoff][xoff].toNumber;
       }
     }
 
-    n -= this.cells[y][x].toNumber();
+    n -= this.cells[y][x].toNumber;
 
     return n;
   }
@@ -129,7 +143,7 @@ let canvas;
 let button;
 
 function setup() {
-  canvas = createCanvas(800, 800);
+  canvas = createCanvas(400, 400);
   canvas.elt.setAttribute('oncontextmenu', 'return false;');
   canvas.mousePressed((e) => {
     let xcol = floor(mouseX / board.cellSize);
@@ -145,10 +159,9 @@ function setup() {
     board.paused = !board.paused;
   });
   background(56);
-  frameRate(60);
-  board = new Board(width, height, 5);
-  board.delay = 2;
-  board.draw();
+   board = new Board(width, height, 20);
+   board.delay = 30;
+   board.draw();
 }
 
 function draw() {
